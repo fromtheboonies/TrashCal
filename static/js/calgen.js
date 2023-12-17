f9b9abfe4d1791612032b84681aba5590d03e7c3
Jesse Schauer <jesseschauer@hotmail.com>
2023-12-17 15:59:04 -0600

    dynamic holidays and checkboxes misc documentation and cleanup

--- static/js/calgen.js
+++ static/js/calgen.js
@@ -1,31 +1,47 @@
-var gDebug = false;
-var lincolnHolidays = [
-    new Date("2024-01-01T00:00:00"), // new years
-    new Date("2024-05-27T00:00:00"), // memorial day
-    new Date("2024-07-04T00:00:00"), // independence day
-    new Date("2024-09-02T00:00:00"), // labor day
-    new Date("2024-11-28T00:00:00"), // thanksgiving
-    new Date("2024-12-25T00:00:00"), // christmas
+var gDebug = false;  // global debug flag - enables detailed logging in functions
+
+class CollectionDay {
+    constructor(date, summary, description, isInHolidayWeek, isTrashOnly) {
+        this.date = date;
+        this.summary = summary;
+        this.description = description;
+        this.isInHolidayWeek = isInHolidayWeek;
+        this.isTrashOnly = isTrashOnly;
+    }
+}
+
+class Holiday {
+    constructor(date, description) {
+        this.date = date;
+        this.description = description;
+    }
+}
+
+/**
+ * list of holidays used for generating checkboxes
+ */
+var holidays = [ 
+    new Holiday(new Date("2024-01-01T00:00:00"), 'New Years Day'),
+    new Holiday(new Date("2024-01-15T00:00:00"), 'Martin Luther King day'),
+    new Holiday(new Date("2024-02-19T00:00:00"), 'President\'s Day'),
+    new Holiday(new Date("2024-03-29T00:00:00"), 'Good Friday'),
+    new Holiday(new Date("2024-05-27T00:00:00"), 'Memorial Day'),
+    new Holiday(new Date("2024-07-04T00:00:00"), 'Independence Day'),
+    new Holiday(new Date("2024-09-02T00:00:00"), 'Labor Day'),
+    new Holiday(new Date("2024-11-11T00:00:00"), 'Veteran\'s Day'),
+    new Holiday(new Date("2024-11-28T00:00:00"), 'Thanksgiving'),
+    new Holiday(new Date("2024-12-25T00:00:00"), 'Christmas')
+
 ];
 
-var holidays = [
-    new Date("2024-01-01T00:00:00"), // new years
-    new Date("2024-01-15T00:00:00"), // Martin Luther King day
-    new Date("2024-02-19T00:00:00"), // President's Day
-    new Date("2024-03-29T00:00:00"), // Good Friday
-    new Date("2024-05-27T00:00:00"), // memorial day
-    new Date("2024-07-04T00:00:00"), // independence day
-    new Date("2024-09-02T00:00:00"), // labor day
-    new Date("2024-11-11T00:00:00"), // Veteran's Day
-    new Date("2024-11-28T00:00:00"), // thanksgiving
-    new Date("2024-12-25T00:00:00"), // christmas
-];
-
+/**
+ * Collection to get day name for selected index value
+ */
 const dayName = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
 var collectionDayName = '';
 
 function trashCal(writeToIcsFile){
-    
+    let debug = gDebug || false;
     let sStartDate = $("#startDate").val();
     let sEndDate = $("#endDate").val();
     let sFirstRecycleDate = $("#firstRecycleCollectionDate").val();
@@ -45,13 +61,11 @@ function trashCal(writeToIcsFile){
     if(daysDifference(startDate, firstRecycleDate) != 0 && !holidayWeekCheck(firstRecycleDate)) {
        prevRecycleDay = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate()-7);
     }
-    
-    
+        
     var nextCollectionDay = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate());
 
     var genRecycleAndHolidayOnly = $('#recycleOnlyInd').val()==='on';
     
-
     var collectionDays = [];
 
     while(nextCollectionDay <= endDate) {
@@ -93,7 +107,10 @@ function trashCal(writeToIcsFile){
         nextCollectionDay = new Date(nextCollectionDay.getFullYear(), nextCollectionDay.getMonth(), nextCollectionDay.getDate()+7);
     }
 
-    console.log('collectionDays: ', collectionDays);
+    if(debug) console.log('collectionDays: ', collectionDays);
+    if(debug) console.table(collectionDays);
+
+    displayEvents(collectionDays);
     if(writeToIcsFile) {
         writeIcsFile(collectionDays);
     }
@@ -101,6 +118,10 @@ function trashCal(writeToIcsFile){
     return collectionDays;
 }
 
+/**
+ * Creates ics file from the provided CollectionDay[]
+ * @param {CollectionDay[]} collectionDays 
+ */
 function writeIcsFile(collectionDays) {
     var output = 
         'BEGIN:VCALENDAR\n' 
@@ -124,7 +145,6 @@ function writeIcsFile(collectionDays) {
         +'DTSTART;VALUE=DATE:'+icalDateFormat(collectionDay.date)+'\n'
         +'DTEND;VALUE=DATE:'+icalDateFormat(collectionDay.date)+'\n'
         +'SUMMARY:'+collectionDay.summary+'\n'
-       // +'LOCATION:Home\n'
         +'DESCRIPTION:'+collectionDay.description+'\n'
         +'END:VEVENT\n';
     });
@@ -141,12 +161,23 @@ function writeIcsFile(collectionDays) {
     document.body.removeChild(elem);
 }
 
+/**
+ * Checks if provided date is in a selected holiday week.
+ * @param {Date} checkDate 
+ * @returns {Boolean} true if provided date is in a selected holiday week.
+ */
 function holidayWeekCheck(checkDate) {
-    var debug = gDebug || false;
-    var isHolidayWeek = false;
-    
-    holidays.forEach(function(holiday) {   
-        var diff = daysDifference(holiday, checkDate);
+    let debug = gDebug || false;
+    let isHolidayWeek = false;
+    let selectedHolidays = getSelectedHolidays();
+
+    if(selectedHolidays.length == 0) {
+        selectedHolidays = holidays;
+    }
+
+    selectedHolidays.forEach(function(holiday) {   
+
+        var diff = daysDifference(holiday.date, checkDate);
         
         if(diff==0) {
             isHolidayWeek = true;
@@ -183,21 +214,42 @@ function holidayWeekCheck(checkDate) {
 
 }
 
-
-
+/**
+ * Calculate the number of days difference between 2 dates (d1-d0)
+ * @param {Date} d0 
+ * @param {Date} d1 
+ * @returns {Number} number of days between 2 provided dates
+ */
 function daysDifference(d0, d1) {
     var diff = new Date(+d1).setHours(12) - new Date(+d0).setHours(12);
     return Math.round(diff/8.64e7);
 }
   
-// Simple formatter
+/**
+ * Formats date value for presentation
+ * @param {Date} date 
+ * @returns date string in 'YYYY-MM-dd' format
+ */
 function formatDate(date){
     return [date.getFullYear(),('0'+(date.getMonth()+1)).slice(-2),('0'+date.getDate()).slice(-2)].join('-');
 }
 
+/**
+ * Formats date value in iCalendar format
+ * @param {Date} date 
+ * @returns 
+ */
 function icalDateFormat(date){
     return [date.getFullYear(),('0'+(date.getMonth()+1)).slice(-2),('0'+date.getDate()).slice(-2)].join('');
 }
+
+/**
+ * Calendar start date will not be equal to first collection date.  
+ * Required for syncing prior collection days and holidays and for 'trash only' holidays.
+ * @param {Date} startDate 
+ * @param {Date} dayOfWeek 
+ * @returns 
+ */
 function initializeStartDateToCollectionDay(startDate, dayOfWeek) {
     var adjStartDate = startDate;
 
@@ -212,6 +264,132 @@ function initializeStartDateToCollectionDay(startDate, dayOfWeek) {
     return adjStartDate;
 }
 
+/**
+ * Generates checkbox fields and labels for list of holidays.
+ */
+function generateHolidayBoxes() {
+    let datesContainer = document.getElementById("holidaysContainer");
+
+    holidays.forEach((holiday) => {
+        let dateContainer = document.createElement("div");
+        dateContainer.classList.add("input-group-text");
+        
+        let dateBox = document.createElement("input");
+        dateBox.type = "checkbox";
+        dateBox.value = holiday.date;
+        dateBox.id = holiday.description.toLowerCase().replace(/[\W']/g, '');
+        dateBox.name = "holidayBox";
+        dateBox.title = formatDate(holiday.date);
+
+        let label = document.createElement("label");
+        label.htmlFor = dateBox.id;
+        label.appendChild(document.createTextNode(holiday.description));
+        label.title = formatDate(holiday.date);
+
+        dateContainer.appendChild(dateBox);
+        dateContainer.appendChild(label);
+
+        datesContainer.appendChild(dateContainer);
+        
+    })
+}
+
+
+/**
+ * Iterates collectionDays collection and draws them as table rows and cells in events table
+ * @param {CollectionDay[]} collectionDays 
+ */
+function displayEvents(collectionDays) {
+    let tbody = $("#eventsTable tbody");
+    tbody.empty(); // clear any rows before repopulating table
+    
+
+    collectionDays.forEach((collectionDay) => {
+        let tr = document.createElement("tr");
+        let d = document.createElement("td");
+
+        d.textContent = formatDate(collectionDay.date);
+        tr.appendChild(d);
+
+        let s = document.createElement("td");
+        s.textContent = collectionDay.summary;
+        tr.appendChild(s);
+
+        let holidayInd = document.createElement("td");
+        holidayInd.textContent = collectionDay.isInHolidayWeek ? "Yes" : "No";
+        tr.appendChild(holidayInd);
+
+        let trashOnlyInd = document.createElement("td");
+        trashOnlyInd.textContent = collectionDay.isTrashOnly ? "Yes" : "No";      
+        tr.appendChild(trashOnlyInd);
+
+        tbody.append(tr);
+    })
+
+}
+
+/**
+ * Get array of holidays that are checked.
+ * @returns {Holiday[]} array of selected Holidays
+ */
+function getSelectedHolidays() {
+    let selectedHolidays = [];
+    try {
+        $('input[name=\"holidayBox\"]').each((i,holiday) => {
+            if(holiday.checked) { 
+                selectedHolidays.push(holidays[i]);
+            }
+        });
+      
+    } catch (error) {
+        console.error('error getting selected holidays', error);
+    }
+    
+    return selectedHolidays;
+}
+
+/**
+ * Initialize holiday selections to known group.
+ * @param {string} holidayGroup 
+ */
+function setHolidaySelectionsByGroup(holidayGroup) {
+    // uncheck all first
+    $('input[name=\"holidayBox\"]').prop('checked', false);
+    
+    switch (holidayGroup) {
+        case "lincoln":
+            setLincolnHolidays();
+            break;
+        case "bismarck":
+            setBismarckHolidays();
+            break;
+        
+    }
+
+}
+
+function setLincolnHolidays() {
+    $('#newyearsday').prop('checked',true);
+    $('#memorialday').prop('checked',true);
+    $('#independenceday').prop('checked',true);
+    $('#laborday').prop('checked',true);
+    $('#thanksgiving').prop('checked',true);
+    $('#christmas').prop('checked',true);
+    
+}
+
+function setBismarckHolidays() {
+    $('#newyearsday').prop('checked',true);
+    $('#martinlutherkingday').prop('checked',true);
+    $('#presidentsday').prop('checked',true);
+    $('#goodfriday').prop('checked',true);
+    $('#memorialday').prop('checked',true);
+    $('#independenceday').prop('checked',true);
+    $('#laborday').prop('checked',true);
+    $('#veteransday').prop('checked',true);
+    $('#thanksgiving').prop('checked',true);
+    $('#christmas').prop('checked',true);
+}
 
 function testHolidayWeekCheck() {
     holidayWeekCheck(new Date("2022-07-03T00:00:00"));
@@ -223,20 +401,10 @@ function testHolidayWeekCheck() {
 }
 
 
-class CollectionDay {
-    constructor(date, summary, description, isInHolidayWeek, isTrashOnly) {
-        this.date = date;
-        this.summary = summary;
-        this.description = description;
-        this.isInHolidayWeek = isInHolidayWeek;
-        this.isTrashOnly = isTrashOnly;
-    }
-}
-
-
-
 $(function(){
-    //$('#startDate').val('2022-01-01');
-//    $('#startDate').datepicker({dateFormat: 'yyyy-mm-dd'});
+    
+    generateHolidayBoxes();
+
+
 })
 
